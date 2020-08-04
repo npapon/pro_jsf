5b6e12e703ba9641ad1fa877f6b019cfa900ef6c
Papoune <npapon@live.fr>
2020-08-04 15:20:19 +0200

    Revert "gameover'"
    This reverts commit 649d98c141350cc8b0ad5b3c7b1de2aff9774d16.

--- pro_jsf/src/Main.java
+++ pro_jsf/src/Main.java
@@ -1,13 +1,30 @@
-import javax.faces.context.FacesContext;
+import javax.ejb.EJB;
+import javax.servlet.http.Cookie;
+
+import cookie.CookieGenerateur;
+import dao.MenuDao;
 
 public class Main {
+
+    @EJB
+    public static MenuDao menuDao;
+
+    public static Cookie setCookieTest2( String nom, String valeur ) {
+
+        return new Cookie( nom, valeur );
+
+    }
 
     public static void main( String[] args ) {
         // TODO Auto-generated method stub
 
-        String version = FacesContext.class.getPackage().getImplementationVersion();
+        Cookie cookie = setCookieTest2( "gay", "lol" );
+        Cookie cookie2 = new Cookie( "test", "test" );
 
-        System.out.println( version );
+        CookieGenerateur cookieGenerateur = new CookieGenerateur();
+
+        System.out.print( cookie2.getValue() );
+        System.out.print( cookie );
     }
 
 }
